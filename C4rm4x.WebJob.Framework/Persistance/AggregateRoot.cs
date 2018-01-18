2c27840fef9cbc5b4e2b061a7fc26ad2f849b79a
Carmelo Quintana <carmeloquintanahernandez@hotmail.com>
2018-01-18 23:02:47 +0000

    Fixing versioning

--- C4rm4x.WebJob.Framework/Persistance/AggregateRoot.cs
+++ C4rm4x.WebJob.Framework/Persistance/AggregateRoot.cs
@@ -51,12 +51,14 @@ namespace C4rm4x.WebJob.Framework
         /// <returns>The collection of events pending to be processed</returns>
         public IEnumerable<JobEventData> FlushEvents()
         {
+            Version++;
+
             var events = _events.ToArray();
 
             foreach (var @event in events)
             {
                 @event.Id = Id;
-                @event.Version = ++Version;
+                @event.Version = Version;
             }
 
             _events.Clear();
