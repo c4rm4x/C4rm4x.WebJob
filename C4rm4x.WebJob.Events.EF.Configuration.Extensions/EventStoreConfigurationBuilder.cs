7e9a18ddaa31a625b92157edec9bae458cb653f4
c4rm4x <c4rm4x@DESKTOP-ONQ0H2J>
2018-12-29 13:12:33 +0000

    Adding configuration builder

---
+++ C4rm4x.WebJob.Events.EF.Configuration.Extensions/EventStoreConfigurationBuilder.cs
@@ -0,0 +1,56 @@
+﻿using C4rm4x.WebJob.Framework;
+
+namespace C4rm4x.WebJob.Events.EF.Configuration
+{
+    /// <summary>
+    /// Configures the EventStoreConfiguration using a fluid interface
+    /// </summary>
+    public class EventStoreConfigurationBuilder
+    {
+        private readonly EventStoreConfiguration _configuration;
+
+        private EventStoreConfigurationBuilder()
+        {
+            _configuration = EventStoreConfiguration.Create();
+        }
+
+        /// <summary>
+        /// Creates a new instance of EventStoreConfigurationBuilder
+        /// </summary>
+        /// <remarks>
+        /// This is the entry point to start configuring a new EventStoreConfiguration
+        /// </remarks>
+        /// <returns>A new instance of EventStoreConfigurationBuilder</returns>
+        public static EventStoreConfigurationBuilder Configure() => new EventStoreConfigurationBuilder();
+
+        /// <summary>
+        /// Flags events of type T as sensitive
+        /// </summary>
+        /// <typeparam name="T">Type of the event to be flagged as sensitive</typeparam>
+        /// <returns>This builder</returns>
+        public EventStoreConfigurationBuilder SensitivePayload<T>() where T : JobEventData
+        {
+            _configuration.SensitivePayload<T>();
+
+            return this;
+        }
+
+        /// <summary>
+        /// Flags given type should be ignored every time an event contains any property of this type
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public EventStoreConfigurationBuilder ShouldIgnore<T>()
+        {
+            _configuration.ShouldIgnore<T>();
+
+            return this;
+        }
+
+        /// <summary>
+        /// Creates a new instance of EventStoreConfiguration with the configuration specified at this moment
+        /// </summary>
+        /// <returns>A new instance of EventStoreConfiguration configured with the specified rules</returns>
+        public IEventStoreConfiguration Build() => _configuration;
+    }
+}
---
+++ C4rm4x.WebJob.Events.EF.Configuration.Extensions/Internal/EventStoreConfiguration.cs
@@ -0,0 +1,68 @@
+﻿using C4rm4x.Tools.Utilities;
+using C4rm4x.WebJob.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace C4rm4x.WebJob.Events.EF.Configuration
+{
+    internal class EventStoreConfiguration : IEventStoreConfiguration
+    {
+        public ICollection<Type> SensitiveEvents { get; private set; }
+
+        public ICollection<Type> TypesToIgnore { get; private set; }
+
+        private EventStoreConfiguration()
+        {
+            SensitiveEvents = new HashSet<Type>();
+            TypesToIgnore = new HashSet<Type>();
+        }
+
+        public static EventStoreConfiguration Create() => new EventStoreConfiguration();
+
+        public bool IsSensitive<T>(T eventData) where T : JobEventData
+        {
+            return IsSensitive(eventData.GetType());
+        }
+
+        public bool IsSensitive(Type eventDataType)
+        {
+            eventDataType.Is<JobEventData>();
+
+            return SensitiveEvents.Contains(eventDataType);
+        }
+
+        IEnumerable<Type> IEventStoreConfiguration.TypesToIgnore => this.TypesToIgnore.AsEnumerable();
+
+
+        internal EventStoreConfiguration SensitivePayload<T>() where T : JobEventData
+        {
+            return SensitivePayload(typeof(T));
+        }
+
+        internal EventStoreConfiguration SensitivePayload(Type eventDataType)
+        {
+            eventDataType.Is<JobEventData>();
+
+            SensitiveEvents.Add(eventDataType);
+
+            return this;
+        }
+
+        internal EventStoreConfiguration ShouldIgnore<T>()
+        {
+            return ShouldIgnore(typeof(T));
+        }
+
+        internal EventStoreConfiguration ShouldIgnore(Type typeToIgnore)
+        {
+            typeToIgnore.Must(t => !typeof(JobEventData).IsAssignableFrom(t),
+                "{0} must not be compatible with {1}".AsFormat(
+                    typeToIgnore.FullName, typeof(JobEventData).FullName));
+
+            TypesToIgnore.Add(typeToIgnore);
+
+            return this;
+        }
+    }
+}
